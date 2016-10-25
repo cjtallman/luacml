a1e04bbc4242f6a1f1e04755cb7007404e365884
Chris Tallman <cjtallman@gmail.com>
2016-10-25 14:25:05 -0700

    deploy script

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-FULL_REPO="https://$GH_TOKEN@github.com/cjtallman/luacml.git"
-
-git remote set-url origin $FULL_REPO
-
 pip install mkdocs
 
-mkdocs gh-deploy --clean
+git config user.name "Chris Tallman"
+git config user.email "${GH_EMAIL}"
+git remote add upstream "https://${GH_TOKEN}@github.com/cjtallman/luacml.git"
+git fetch upstream && git fetch upstream gh-pages:gh-pages
+
+mkdocs gh-deploy -v --clean --remote-name upstream
